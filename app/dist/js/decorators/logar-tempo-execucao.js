87cb65371c1c1357fc35c0c16be136d42c4e2178
Tiago Reolon Vazzoller <89028876+tiagorv0@users.noreply.github.com>
2022-03-07 01:03:23 -0300

    add projeto

---
+++ app/dist/js/decorators/logar-tempo-execucao.js
@@ -0,0 +1,19 @@
+export function logarTempoDeExecucao(emSegundos = false) {
+    return function (target, propertyKey, descriptor) {
+        const metodoOriginal = descriptor.value;
+        descriptor.value = function (...args) {
+            let divisor = 1;
+            let unidade = 'milisegundos';
+            if (emSegundos) {
+                divisor = 1000;
+                unidade = 'segudos';
+            }
+            const t1 = performance.now();
+            const retorno = metodoOriginal.apply(this, args);
+            const t2 = performance.now();
+            console.log(`${propertyKey}, tempo de execução: ${(t2 - t1) / divisor} ${unidade}`);
+        };
+        return descriptor;
+    };
+}
+//# sourceMappingURL=logar-tempo-execucao.js.map
---
+++ app/dist/js/views/view.js
@@ -0,0 +1,17 @@
+export class View {
+    constructor(seletor) {
+        this.escapar = false;
+        const elemento = document.querySelector(seletor);
+        if (elemento) {
+            this.elemento = elemento;
+        }
+        else {
+            throw Error(`Seletor ${seletor} não existe no DOM. Verifique`);
+        }
+    }
+    update(model) {
+        let template = this.template(model);
+        this.elemento.innerHTML = template;
+    }
+}
+//# sourceMappingURL=view.js.map
